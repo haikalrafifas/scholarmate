a567ab17829b37231889acc174b7efe82fa86a0d
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-07-02 12:23:37 +0700

    complete the making of Beasiswa page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { DetailJurusan } from "./pages/DetailJurusan/DetailJurusan";
+import { DetailBeasiswa } from "./pages/DetailBeasiswa/DetailBeasiswa";
 // selalu hapus import code yang ingin dites dari direktori lain.
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* Mulai masukkan code yang ingin dites di bawah baris ini */}
-        <DetailJurusan />
+        <DetailBeasiswa />
       </header>
     </div>
   );
