16233260c07ab8b9eb4c431f2f6ff24c58200214
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-07-03 21:07:06 +0700

    refactor route, controller, and model
    In this commit, the routes, controllers, and models for the scholarship data have been refactored to make them tidier. The most obvious change can be seen from the addition of various functions that are tasked with handling requests from users.

--- server/controllers/scholarshipsController.js
+++ server/controllers/scholarshipsController.js
@@ -4,175 +4,249 @@ const getScholarshipsInfo = async (req, res, next) => {
   const { degree, negara_tujuan, tipe, urut_berdasarkan } = req.query;
 
   if (degree && negara_tujuan && tipe && urut_berdasarkan) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByDegreeAndCountryAndTypeAndOrderBy(
-          degree,
-          negara_tujuan,
-          tipe,
-          urut_berdasarkan
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByDegreeAndCountryAndTypeAndOrderBy(req, res);
   }
 
   if (tipe && urut_berdasarkan) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByTypeAndOrderBy(
-          tipe,
-          urut_berdasarkan
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByTypeAndOrderBy(req, res);
   }
 
   if (negara_tujuan && urut_berdasarkan) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByCountryAndOrderBy(
-          negara_tujuan,
-          urut_berdasarkan
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByCountryAndOrderBy(req, res);
   }
 
   if (negara_tujuan && tipe) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByCountryAndType(
-          negara_tujuan,
-          tipe
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByCountryAndType(req, res);
   }
 
   if (degree && urut_berdasarkan) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByDegreeAndOrderBy(
-          degree,
-          urut_berdasarkan
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByDegreeAndOrderBy(req, res);
   }
 
   if (degree && tipe) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByDegreeAndType(degree, tipe);
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      res.status(204);
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByDegreeAndType(req, res);
   }
 
   if (degree && negara_tujuan) {
-    try {
-      let [scholarships, _] =
-        await ScholarshipModel.getScholarshipByDegreeAndCountry(
-          degree,
-          negara_tujuan
-        );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByDegreeAndCountry(req, res);
   }
 
   if (degree) {
-    try {
-      let [scholarships, _] = await ScholarshipModel.getScholarshipByDegree(
-        degree
-      );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByDegree(req, res);
   }
 
   if (negara_tujuan) {
-    try {
-      let [scholarships, _] = await ScholarshipModel.getScholarshipByCountry(
-        negara_tujuan
-      );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByCountry(req, res);
   }
 
   if (tipe) {
-    try {
-      let [scholarships, _] = await ScholarshipModel.getScholarshipByType(tipe);
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
+    return getScholarshipByType(req, res);
   }
 
   if (urut_berdasarkan) {
-    try {
-      let [scholarships, _] = await ScholarshipModel.getScholarshipOrderBy(
-        urut_berdasarkan
-      );
-
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      res.status(204);
-      next(error);
-    }
+    return getScholarshipOrderBy(req, res);
   }
 
   if (!degree && !negara_tujuan && !tipe) {
-    try {
-      const scholarships = await ScholarshipModel.getAllScholarships();
+    return getAllScholarships(req, res);
+  }
+};
 
-      return res.status(200).json(scholarships);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
+const getScholarshipByDegreeAndCountryAndTypeAndOrderBy = async (
+  req,
+  res,
+  next
+) => {
+  const { degree, negara_tujuan, tipe, urut_berdasarkan } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByDegreeAndCountryAndTypeAndOrderBy(
+        degree,
+        negara_tujuan,
+        tipe,
+        urut_berdasarkan
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByTypeAndOrderBy = async (req, res, next) => {
+  const { tipe, urut_berdasarkan } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByTypeAndOrderBy(
+        tipe,
+        urut_berdasarkan
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByCountryAndOrderBy = async (req, res, next) => {
+  const { negara_tujuan, urut_berdasarkan } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByCountryAndOrderBy(
+        negara_tujuan,
+        urut_berdasarkan
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByCountryAndType = async (req, res, next) => {
+  const { negara_tujuan, tipe } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByCountryAndType(
+        negara_tujuan,
+        tipe
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByDegreeAndOrderBy = async (req, res, next) => {
+  const { degree, urut_berdasarkan } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByDegreeAndOrderBy(
+        degree,
+        urut_berdasarkan
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByDegreeAndType = async (req, res, next) => {
+  const { degree, tipe } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByDegreeAndType(degree, tipe);
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    res.status(204);
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByDegreeAndCountry = async (req, res, next) => {
+  const { degree, negara_tujuan } = req.query;
+
+  try {
+    let [scholarships, _] =
+      await ScholarshipModel.getScholarshipByDegreeAndCountry(
+        degree,
+        negara_tujuan
+      );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByDegree = async (req, res, next) => {
+  const { degree } = req.query;
+
+  try {
+    let [scholarships, _] = await ScholarshipModel.getScholarshipByDegree(
+      degree
+    );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByCountry = async (req, res, next) => {
+  const { negara_tujuan } = req.query;
+
+  try {
+    let [scholarships, _] = await ScholarshipModel.getScholarshipByCountry(
+      negara_tujuan
+    );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipByType = async (req, res, next) => {
+  const { tipe } = req.query;
+
+  try {
+    let [scholarships, _] = await ScholarshipModel.getScholarshipByType(tipe);
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getScholarshipOrderBy = async (req, res, next) => {
+  const { urut_berdasarkan } = req.query;
+
+  try {
+    let [scholarships, _] = await ScholarshipModel.getScholarshipOrderBy(
+      urut_berdasarkan
+    );
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    res.status(204);
+    next(error);
+  }
+};
+
+const getAllScholarships = async (req, res, next) => {
+  try {
+    const scholarships = await ScholarshipModel.getAllScholarships();
+
+    return res.status(200).json(scholarships);
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
 
--- server/routes/api/scholarships.js
+++ server/routes/api/scholarships.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const scholarshipsController = require("../../controllers/scholarshipsController");
 
-const getParamInfo = (req, res, next) => {
-  res.send(req.query);
-};
-
 router.route("/").get(scholarshipsController.getScholarshipsInfo);
 
 module.exports = router;
