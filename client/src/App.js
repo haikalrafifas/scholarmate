9ad0824f4f49afd97721db94c492824689f5f936
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2023-07-04 13:00:18 +0700

    Perubahan terakhir front-end
    Website ini belum responsif

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./App.css";
-import { ProfilJurusan } from "./pages/ProfilJurusan/ProfilJurusan";
+// import { CareerPage } from "./pages/CareerPage/CareerPage"
+// import { DetailBeasiswa } from "./pages/DetailBeasiswa/DetailBeasiswa"
+// import { DetailJurusan } from "./pages/DetailJurusan/DetailJurusan"
+// import { HomePages } from "./pages/HomePages/HomePages"
+// import { LoginSuccessPages } from "./pages/LoginSuccessPages/LoginSuccessPages"
+// import { ProfilJurusan  } from "./pages/ProfilJurusan/ProfilJurusan"
+//  import { LoginPage } from "./pages/LoginPage/LoginPage"
+// import { SignupPage } from "./pages/SignupPage/SignupPage"
+// import { ProfilJurusanView } from "./pages/ProfilJurusanView/ProfilJurusanView";
+// import {Beasiswa } from "./pages/Beasiswa/Beasiswa"
+// import { ProfilPenggunaSimpanBeasiswa } from "./pages/ProfilPenggunaSimpanBeasiswa/ProfilPenggunaSimpanBeasiswa"
+// import { ProfilPenggunaEditPreferensi } from "./pages/ProfilPenggunaEditPreferensi/ProfilPenggunaEditPreferensi"
+// import { ProfilPenggunaLihatPreferensi } from "./pages/ProfilPenggunaLihatPreferensi/ProfilPenggunaLihatPreferensi"
+
 // selalu hapus import code yang ingin dites dari direktori lain.
 
 function App() {
@@ -8,7 +21,45 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* Mulai masukkan code yang ingin dites di bawah baris ini */}
-        <ProfilJurusan />
+        {/* <Beasiswa /> */}
+        {/* DONE */}
+
+        {/* <CareerPage/> */}
+        {/* DONE */}
+
+        {/* <DetailBeasiswa /> */}
+        {/* DONE */}
+
+        {/* <DetailJurusan/> */}
+        {/* DONE  */}
+
+        {/* <HomePages/> */}
+        {/* WORK 80% */}
+
+        {/* <LoginPage/> */}
+        {/* DONE */}
+
+        {/* <LoginSuccessPages/> */}
+        {/* DONE */}
+
+        {/* <SignupPage/>  */}
+        {/* DONE */}
+
+        {/* <ProfilJurusan/> */}
+        {/* DONE  */}
+
+        {/* <ProfilJurusanView/> */}
+        {/* DONE */}
+
+        {/* <ProfilPenggunaSimpanBeasiswa/>  */}
+        {/* DONE */}
+
+        {/* <ProfilPenggunaEditPreferensi/> */}
+        {/* 80 %*/}
+
+        {/* <ProfilPenggunaLihatPreferensi/> */}
+        {/* DONE */}
+
       </header>
     </div>
   );
