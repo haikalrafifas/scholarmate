f2940d7bd40d7bad770b1e2bd0215e9f4372d006
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2023-07-05 19:39:49 +0700

    update card jurusan

--- client/src/components/CardJurusan/CardJurusan.jsx
+++ client/src/components/CardJurusan/CardJurusan.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import "./style.css";
 
-export const CardJurusan = ({ image, description }) => {
+export const CardJurusan = ({ className, image, description }) => {
   return (
-    <div className="card">
+    <div className={`card-jurusan ${className}`}>
       <div className="card-image">
         <img src={image} alt="Card" />
       </div>
       <div className="card-description">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, voluptatem saepe! Ea voluptas blanditiis maiores libero sequi officiis amet ipsa culpa nihil deleniti minima, mollitia ullam dolore voluptatibus, fugiat natus.</p>
+        <p>{description}</p>
       </div>
     </div>
   );
--- client/src/pages/ProfilJurusanView/ProfilJurusanView.jsx
+++ client/src/pages/ProfilJurusanView/ProfilJurusanView.jsx
@@ -1,38 +1,50 @@
-import React from "react";
-import { CardJurusan } from "../../components/CardJurusan/CardJurusan";
-import { NavNavbar } from "../../components/NavNavbar/NavNavbar";
-import "./style.css";
+  import React from "react";
+  import { CardJurusan } from "../../components/CardJurusan/CardJurusan";
+  import { NavNavbar } from "../../components/NavNavbar/NavNavbar";
+  import "./style.css";
 
-export const ProfilJurusanView = () => {
-  return (
-    <div className="PROFIL-JURUSAN-VIEW">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <div className="overlap-group">
-            <div className="rectangle" />
-            <div className="div">
-              <div className="ellipse" />
-              <div className="text-wrapper">RUMPUN ILMU KOMPUTER</div>
-              <h1 className="h-1">Profil Jurusan</h1>
-              <NavNavbar
-                className="nav-navbar-instance"
-                logoLogoClassName="design-component-instance-node"
-                property1="with-avatar"
+  export const ProfilJurusanView = () => {
+    return (
+      <div className="PROFIL-JURUSAN-VIEW">
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <div className="overlap-group">
+              <div className="rectangle" />
+              <div className="div">
+                <div className="ellipse" />
+                <div className="text-wrapper">RUMPUN ILMU KOMPUTER</div>
+                <h1 className="h-1">Profil Jurusan</h1>
+                <NavNavbar
+                  className="nav-navbar-instance"
+                  logoLogoClassName="design-component-instance-node"
+                  property1="with-avatar"
+                />
+              </div>
+            </div>
+            <div className="card-jurusan-wrapper">
+              <CardJurusan
+                className="card-rumpun-instance"
+                image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
+                description="lorem ipsum"
+              />
+              <CardJurusan
+                className="card-rumpun-2"
+                image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
+                description="lorem ipsum"
+              />
+              <CardJurusan
+                className="card-rumpun-3"
+                image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
+                description="lorem ipsum"
+              />
+              <CardJurusan
+                className="card-rumpun-4"
+                image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
+                description="lorem ipsum"
               />
             </div>
           </div>
-          <CardJurusan
-            className="card-rumpun-instance"
-            image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
-            description="lorem ipsum"
-          />
-          <CardJurusan
-            className="card-rumpun-1"
-            image="https://generation-sessions.s3.amazonaws.com/da4e0e1008ef017d1ac917b5bcb034ff/img/unsplash-uip163xcv6w-1@2x.png"
-            description="lorem ipsum"
-          />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
